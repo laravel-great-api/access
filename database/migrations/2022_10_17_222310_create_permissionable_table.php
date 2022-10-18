a1e8837b5cf25c9822ab215e642d7d545723901e
walshdev <alexwalsh.dev@gmail.com>
2022-10-18 18:11:20 +0300

    upd

--- database/migrations/2022_10_17_222310_create_permissionable_table.php
+++ database/migrations/2022_10_17_222310_create_permissionable_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('permissionable');
+        Schema::dropIfExists('permissionables');
     }
 };
